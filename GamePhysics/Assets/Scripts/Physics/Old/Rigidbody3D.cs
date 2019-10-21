38935a5c24afb8226530203ee902d6764dfad4f8
Elzanaty <karimelzanaty98@gmail.com>
2019-10-21 10:48:11 -0400

    Started Rigidbody 3D

---
+++ GamePhysics/Assets/Scripts/Physics/Old/Rigidbody3D.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rigidbody3D : MonoBehaviour
+{
+	private Vector3 force = Vector3.zero;
+
+	public Vector3 Position;
+	public Vector3 PrevPosition;
+	private Vector3 PosDiff = Vector3.zero;
+
+	public Vector3 Velocity;
+	public Vector3 Acceleration;
+
+	public Quaternion Rotation;
+	public Quaternion RotVelocity;
+	public Quaternion RotAcceleration;
+	public Vector3 helperRot;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
