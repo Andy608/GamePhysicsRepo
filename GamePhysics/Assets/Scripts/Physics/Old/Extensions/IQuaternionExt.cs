4dc8951824c5afc0dff2eaf45a59e6a67eb9a6d9
Elzanaty <karimelzanaty98@gmail.com>
2019-10-24 10:53:59 -0400

    Added quat extension and updated rb script

---
+++ GamePhysics/Assets/Scripts/Physics/Old/Extensions/IQuaternionExt.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class QuaternionExt
+{
+	public static Quaternion ScalarQuat(Quaternion quat, float scalar)
+	{
+		quat.x *= scalar;
+		quat.y *= scalar;
+		quat.z *= scalar;
+		quat.w *= scalar;
+		return quat;
+	}
+
+	public static Quaternion MultQuat(Quaternion q1, Quaternion q2)
+	{
+		//1. Break quaternions into variables, vectors, and real
+		Vector3 v1 = new Vector3(q1.x, q1.y, q1.z);
+		Vector3 v2 = new Vector3(q2.x, q2.y, q2.z);
+		float w1 = q1.w;
+		float w2 = q2.w;
+
+		//2. calculate real part
+		//-2.1 Get dot product of vectors
+		float vDot = Vector3.Dot(v1, v2);
+
+		float wFinal = w1 * w2 - vDot;
+
+		//3. calculate vector component
+		//-3.1 Get cross of vectors
+		Vector3 vCross = Vector3.Cross(v1, v2);
+
+		Vector3 vFinal = w1 * v2 + w2 * v1 + vCross;
+
+		Quaternion qFinal = new Quaternion(vFinal.x, vFinal.y, vFinal.z, wFinal);
+
+		return qFinal;
+	}
+}
--- GamePhysics/Assets/Scripts/Physics/Old/Rigidbody3D.cs
+++ GamePhysics/Assets/Scripts/Physics/Old/Rigidbody3D.cs
@@ -11,22 +11,84 @@ public class Rigidbody3D : MonoBehaviour
 	private Vector3 PosDiff = Vector3.zero;
 
 	public Vector3 Velocity;
+	[SerializeField] private Vector2 InitialVel = Vector2.zero;
 	public Vector3 Acceleration;
 
 	public Quaternion Rotation;
-	public Quaternion RotVelocity;
-	public Quaternion RotAcceleration;
+	public Vector3 RotVelocity;
+	public Vector3 RotAcceleration;
 	public Vector3 helperRot;
+
+	[SerializeField] private float startingMass = 1.0f;
+
+	public float Mass { get { return mass; } private set { mass = value > 0.0f ? value : 0.0f; MassInv = mass > 0.0f ? 1.0f / mass : 0.0f; } }
+	private float mass = 1.0f;
+	public float MassInv { get; private set; }
+
+
+	/// <summary>
+	/// Quick direct changes to Velocity.
+	/// </summary>
+	/// <param name="v"></param>
+	public void SetVelocity(Vector2 v)
+	{
+		Velocity = v;
+	}
+	/// <summary>
+	/// Quick direct changes to Position.
+	/// </summary>
+	/// <param name="v"></param>
+	public void SetPosition(Vector2 p)
+	{
+		Position = p;
+	}
+
+	public void AddForce(Vector3 newForce)
+	{
+		//D'Alembert
+		force += newForce;
+	}
 
 	// Start is called before the first frame update
 	void Start()
     {
-        
-    }
+		Mass = startingMass;
+		Position = transform.position;
+		PrevPosition = transform.position;
 
-    // Update is called once per frame
-    void Update()
+		Velocity = InitialVel;
+
+	}
+
+	// Update is called once per frame
+	void Update()
     {
         
     }
+
+	//Pre: Implement operators for multiplying quaternion by scalar
+	//Pre: Implement function for multiplying 3d vector by quaternion
+	//1.Implement Euler for velocity and position
+	//2.Implement Euler for rotation and angular velocity
+	//3.Implement Kinematic for velocity and position 
+	//Bonus: Implement Kinematic for rotation and angular velocity
+	//Bonus: Implement our own quaternion class
+
+	/// <summary> Integrates the particles position using the euler explicit formula </summary>
+	/// <param name="dt"></param>
+	private void UpdatePositionEulerExplicit(float dt)
+	{
+		Position += Velocity * dt;
+		Velocity += Acceleration * dt;
+	}
+	/// <summary> Integrates the particles position using the kinematic formula </summary>
+	/// <param name="dt"></param>
+	private void UpdatePositionKinematic(float dt)
+	{
+		Position += Velocity * dt + 0.5f * Acceleration * dt * dt;
+		Velocity += Acceleration * dt;
+	}
+
+
+
 }
