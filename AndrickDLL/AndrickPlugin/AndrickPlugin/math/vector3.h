f2894b24513686e712958a71dacac1162bb5b900
Elzanaty <karimelzanaty98@gmail.com>
2019-11-21 10:23:28 -0500

    Added Vec3 and 4 base

---
+++ AndrickDLL/AndrickPlugin/AndrickPlugin/math/vector3.h
@@ -0,0 +1,15 @@
+#ifndef VECTOR3_H
+#define VECTOR3_H
+
+class Vector3
+{
+private:
+	float x = 0, y = 0, z = 0;
+
+
+public:
+	Vector3(int x, int y, int z) { this->x = x; this->y = y; this->z = z;};
+
+};
+
+#endif //!VECTOR4_H
---
+++ AndrickDLL/AndrickPlugin/AndrickPlugin/math/vector4.h
@@ -0,0 +1,15 @@
+#ifndef VECTOR4_H
+#define VECTOR4_H
+
+class Vector4
+{
+private:
+	float x = 0, y = 0, z = 0, w = 0;
+
+
+public:
+	Vector4(int x, int y, int z, int w) { this->x = x; this->y = y; this->z = z; this->w = w;};
+
+};
+
+#endif //!VECTOR4_H
