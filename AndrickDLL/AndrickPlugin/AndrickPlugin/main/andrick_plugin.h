09e7330194b2ab22d452b871a0487403ff2cc435
Andrew Rimpici <AndrewR608@gmail.com>
2019-11-21 10:10:28 -0500

    Setup for AndrickDLL

---
+++ AndrickDLL/AndrickPlugin/AndrickPlugin/main/andrick_plugin.h
@@ -0,0 +1,29 @@
+#ifndef ANDRICK_PLUGIN_H_
+#define ANDRICK_PLUGIN_H_
+
+#include "lib.h"
+
+// If C++ is being used in the DLL
+#ifdef __cplusplus
+extern "C"
+{
+#else
+
+// C code
+
+#endif // End __cplusplus
+
+// C++ code exposed to user program.
+
+
+#ifdef __cplusplus // Start __cplusplus
+}
+#else
+
+// C code
+
+#endif // End __cplusplus
+
+// C++ Mode
+
+#endif // End ANDRICKPLUGIN_H_
